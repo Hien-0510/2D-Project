198f4569ba35792ce4af4bc0e1fb23e2b21069fb
Cupid <hien.lm1452@sinhvien.hoasen.edu.vn>
2023-10-19 08:55:41 +0700

    2dProject

---
+++ Assets/Scirps/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rigid;
+    public SpriteRenderer spriteRenderer;
+    public Animator anim;
+
+    private float hInput;
+    private float vInput;
+    private bool IsGrounded;
+
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        UpdateMovement();
+        UpdateFacing();
+        UpdateJumping();
+    }
+
+    private void UpdateMovement()
+    {
+        rigid.velocity = new Vector2(hInput * speed, rigid.velocity.y);
+        anim.SetBool("IsRunning", hInput !=0);
+    }
+
+    private void UpdateJumping()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, speed);
+            IsGrounded = false;
+        }
+        anim.SetBool("IsGrounded", IsGrounded);
+    }
+
+    public void UpdateFacing()
+    {
+        if(hInput != 0)
+        {
+            spriteRenderer.flipX = hInput < 0;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D colision)
+    {
+        if(colision.collider.CompareTag("Ground"))
+        {
+            IsGrounded = true;
+        }
+    }
+}
---
+++ Assets/Scirps/YouWin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouWin : MonoBehaviour
+{
+    public GameObject gameWinPanel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Time.timeScale = 0;
+        gameWinPanel.SetActive(true);
+    }
+}
